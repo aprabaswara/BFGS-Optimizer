33cc74c2f59f6bfbdc47a5d496128968bfea02b3
unknown <aprabaswara@gmail.com>
2021-11-09 03:52:22 +0000

    WG21P4.R

---
+++ WG21P4.R
@@ -0,0 +1,11 @@
+##Group member:
+##1. Aditya Prabaswara Mardjikoen(s2264710)
+##2. Xiangtian Duan(s2248742)
+##3. Huiying Chen(s2264943)
+
+## Github repositories link: 
+## https://github.com/aprabaswara/BFGS-Optimizer.git
+
+bfgs <- function(theta, f, ..., tol, fscale, maxit){
+  list(f, theta, iter, g, H)
+}
