70f2be6d78f185a7ef2f430884786c7bf6c35823
unknown <aprabaswara@gmail.com>
2021-11-15 21:06:19 +0000

    WG21P4.R

--- WG21P4.R
+++ WG21P4.R
@@ -8,6 +8,8 @@
 
 ##overview:
 ##---------------------------------------------------------------------------------------------------------------------------------------------
+
+##test function that professor provided to test the BFGS method
 rb <- function(theta,getg=FALSE,k=10) {
   ## Rosenbrock objective function, suitable for use by 'bfgs'
   z <- theta[1]; x <- theta[2]
@@ -64,35 +66,86 @@ bfgs <- function(theta, f, ..., tol, fscale, maxit){
     return(grad)
   }
   
-  #check if derivative is finite or not
-  if (g(theta,f,...) %in% c(-Inf,Inf) | is.na(g(theta,f,...))){
+  step_reduce <- function(theta, delta, g, f, ...){
+    ##function to reduce the step until satisfying the second Wolfe condition
+    
+    ##input: 
+    ##theta = vector of initial values for the optimization parameter; f = objective function to minimize;
+    ##... = any argument of f after the parameter vector and gradient logical indicator; g = gradient function
+    ##delta = the perturbation vector that we want to reduce
+    
+    ##output: delta = the perturbation vector that satisfying the second Wolfe condition and reduce the values of objective function
+    
+    c2 = 0.9 ##constant for checking the second Wolfe condition
+    grad_delta1 <- crossprod(g(theta,f,...),delta) ##grad(theta)^T delta
+    grad_delta1 <- drop(grad_delta1)
+    grad_delta2 <- crossprod(g(theta+delta,f,...),delta) ##grad(theta+delta)^T delta
+    grad_delta2 <- drop(grad_delta2)
+    
+    ##find delta that satisfying second Wolfe condition and decreases the objective function
+    while (grad_delta2 < c2 * grad_delta1){
+      delta <- delta/2 ##halve the delta
+      grad_delta1 <- crossprod(g(theta,f,...),delta) ##update grad(theta)^T delta
+      grad_delta1 <- drop(grad_delta1)
+      grad_delta2 <- crossprod(g(theta+delta,f,...),delta) ##update grad(theta+delta)^T delta
+      grad_delta2 <- drop(grad_delta2)
+    }
+    
+    return(delta)
+    
+  }
+  
+  
+  ##check if objective function is finite or not
+  if (f(theta,...) %in% c(-Inf,Inf) | is.na(f(theta,...))){
+    warning("Objective function is not finite at given initial theta value!")
+  }
+  
+  ##check if derivative is finite or not
+  else if (g(theta,f,...) %in% c(-Inf,Inf) | is.na(g(theta,f,...))){
     warning("Derivative is not finite at given initial theta value!")
   }
   
-  c1 = 1e-4 #constant for checking the first Wolfe condition
-  c2 = 0.9 #constant for checking the second Wolfe condition
-  n <- length(theta) #length of vector theta
-  B <- diag(n) #initial value of the inverse of Hessian matrix
+  n <- length(theta) ##length of vector theta
+  I <- diag(n) ##initialize identity matrix
+  B <- I ##initial value of the inverse of Hessian matrix
   
-  iter = 1 #initialize first iteration
+  iter = 0 ##initialize first iteration
   
   while(iter <= maxit){
+    iter = iter+1 ##increase iteration by one
     
-    #take the absolute value of each element in f and its gradient
+    ##take the absolute value of each element in f and its gradient
     abs_grad <- abs(g(theta,f,...)) 
     abs_f <- abs(f(theta,...)) 
     
-    #exit the loop if the maximum of iteration is one
-    if (maxit == 1){
+    ##exit the loop if the convergence is reached
+    if (max(abs_grad) < (abs_f+fscale)*tol){
       break
     }
     
     delta <- -B %*% g(theta,f,...) ##compute step length
     delta <- drop(delta) ##return delta as a vector
-    iter = iter+1 ##increase iteration by one
+    delta <- step_reduce(theta, delta, g, f, ...) ##find delta that reduce objective function
+    
+    theta_new <- theta+delta ##update the theta value
+    s <- theta_new-theta
+    y <- g(theta_new,f,...)-g(theta,f,...)
+    inv_rho <- crossprod(s,y) ##rho^(-1)=s^T*y
+    inv_rho <- drop(inv_rho)
+    rho <- 1/sty ##1/rho^(-1)
+    
+    rho_syt <- tcrossprod(s,y) ##rho*s*y^T
+    rho_yst <- tcrossprod(y,s) ##rho*y*s^T
+    
+    matrix1 <- I-rho_syt ##I-rho*s*y^T
+    matrix2 <- I-rho_yst ##I-rho*y*s^T
+    
+    B <- (matrix1 %*% B) %*% matrix2 + rho*tcrossprod(s) ##update B
+    theta <- theta_new ##update theta for the next iteration
   }
   
-  #check if maximum iteration is reached without convergence
+  ##check if maximum iteration is reached without convergence
   if (iter == maxit & max(abs_grad) => (abs_f+fscale)*tol){
     warning("Maximum iteration is reached without convergence!")
   } 
@@ -107,7 +160,7 @@ bfgs <- function(theta, f, ..., tol, fscale, maxit){
     H[i,] <- (g1 - g0)/eps ## approximate second derivatives in Hessian matrix
   }
   
-  #convert asymmetric Hessian matrix into symmetric Hessian matrix
+  ##convert asymmetric Hessian matrix into symmetric Hessian matrix
   if (isSymmetric(H)==FALSE){
     H <- 0.5 * (t(H) + H)
   }
